50d199ce87e7ed599aefa9a992c64d76b8eef583
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:44:02 +0200

    Add files via upload

---
+++ ZRozw1-4/ZRozw1-4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1 = 5;
+            int liczba2 = -3;
+            double liczba3 = -3.2334;
+            double liczba4 = 6.323;
+            float liczba5 = -9;
+            float liczba6 = 12.2323f;
+
+            Console.WriteLine(liczba1 - liczba2);
+            Console.WriteLine(liczba1 + liczba3);
+            Console.WriteLine(liczba1 * liczba5);
+            Console.WriteLine(liczba4 / liczba6);
+            Console.WriteLine(liczba2 % liczba5);
+            Console.ReadKey();
+        }
+    }
+}
